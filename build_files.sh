503dd46b0d9e4794e7efb6033ea4e557cb23aca8
ankit-kumar-art <developerankit213@gmail.com>
2026-09-02 20:17:15 +0530

    fix build script: use python3 instead of python3.9

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,8 @@
-echo " BUILD START"
-python3.9 -m ensurepip --upgrade
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo " BUILD END"
+#!/bin/bash
+set -e
+
+echo "BUILD START"
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
+echo "BUILD END"
