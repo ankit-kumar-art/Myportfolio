8f2a63e7fee79344c975f1b31b1e4adb2e83d875
ankit-kumar-art <developerankit213@gmail.com>
2026-09-02 15:05:16 +0530

    ensure pip is available in static build step

--- build_files.sh
+++ build_files.sh
@@ -2,3 +2,9 @@ echo " BUILD START"
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo " BUILD END" 
+echo " BUILD START"
+python3.9 -m ensurepip --upgrade
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo " BUILD END"
